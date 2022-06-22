18fe2e7abe7e61201b4458015311b5a08d9a6bf4
anupamkumar1 <anupamsingh12@gmail.com>
2022-06-22 19:36:55 +0530

    changed static url

--- cardiovascular/urls.py
+++ cardiovascular/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from api import views
 
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
@@ -14,4 +15,4 @@ urlpatterns = [
     path('api/v2/news/sentiment', views.newsAnanlyserView),
     path('api/v2/all/stats', views.allStats),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
